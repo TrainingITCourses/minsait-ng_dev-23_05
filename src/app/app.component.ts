6a40d2d8e2b6428af22f8ee003367768b2c84945
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-03 18:59:41 +0200

    feat: session 1 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'minsait-ng_dev-23_05';
+  getYear(): number {
+    return new Date().getFullYear();
+  }
 }
