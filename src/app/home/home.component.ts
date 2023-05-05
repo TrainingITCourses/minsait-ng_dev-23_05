6b6ecce03070672893b14b47ed44f4fb47bb1318
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-05 12:24:21 +0200

    feat: component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,30 @@ import { ACTIVITIES } from '../data/activities.data';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  publishedActivities = ACTIVITIES;
+  publishedActivities = ACTIVITIES.filter((a) => a.state === 'published');
+
+  private sortOrder: number = 1;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  isCheap(activity: any): boolean {
+    return activity.price < 100;
+  }
+
+  getClasses(activity: any): string {
+    return activity.ageCategory;
+  }
+
+  onButtonClick() {
+    this.sortOrder = this.sortOrder * -1;
+    this.sortActivities();
+  }
+
+  private sortActivities() {
+    this.publishedActivities.sort(
+      (a, b) => (a.price - b.price) * this.sortOrder
+    );
+  }
 }
