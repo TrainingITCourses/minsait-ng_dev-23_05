6261749272ce4c300f7c4f3023977a07bbf001ca
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-10 16:39:44 +0200

    feat: home about routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeHeaderComponent } from './home-header/home-header.component';
 import { HomeComponent } from './home/home.component';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HomeHeaderComponent],
+  declarations: [AppComponent, HomeComponent, HomeHeaderComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
