4ad5ed4f3b8f2fbb8dca2dbdd23abba361e59086
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-12 09:51:18 +0200

    docs: live

--- src/app/activities/activity-detail/activity-detail-routing.module.ts
+++ src/app/activities/activity-detail/activity-detail-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivityDetailComponent } from './activity-detail.component';
 
-const routes: Routes = [{ path: '', component: ActivityDetailComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ActivityDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ActivityDetailRoutingModule { }
+export class ActivityDetailRoutingModule {}
